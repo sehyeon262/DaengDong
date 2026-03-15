30ea43e87bea8522235de1b3ee06f68242aef948
심다인 <mong2822@naver.com>
2026-03-16 00:43:35 +0900

    [S14P21E108-35] FE/feat: 로그인 도메인 모델 정의

---
+++ frontend/app/src/main/java/com/frontend/domain/model/LoginRequest.kt
@@ -0,0 +1,11 @@
+package com.frontend.domain.model
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)package com.frontend.domain.model
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
--- frontend/app/src/main/java/com/frontend/ui/theme/Color.kt
+++ frontend/app/src/main/java/com/frontend/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.frontend.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Background = Color(0xFFFFFEFB)  // 배경 기본
+val White = Color(0xFFFFFFFF)   // 흰색
+val InputBackground = Color(0xFFEAF0FB) // 입력창 회색 배경
+val PointGreen = Color(0xFFA2CB8B)  // 포인트 색상
+val TextMain = Color(0xFF404040)    // 메인 텍스트 색상
+val TextGray = Color(0xFFE5E7EB)    // 텍스트 회색(연한거)
+val TextBrown = Color(0xFFAF9C9C)   // 텍스트 갈색
+val IconGray = Color(0xFFAAAAAA)    // 아이콘 회색
