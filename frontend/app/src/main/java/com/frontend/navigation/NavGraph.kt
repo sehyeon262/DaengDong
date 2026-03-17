6d9f51f661be28874c70deb96de31ec4812c0fd0
김세현 <sshyeoniz@naver.com>
2026-03-17 10:42:49 +0900

    [S14P21E108-54] FE/feat: 산책산책 전 산책 시ì]  FE/feat: 전 지도 화면 UI 구현

--- frontend/app/src/main/java/com/frontend/navigation/NavGraph.kt
+++ frontend/app/src/main/java/com/frontend/navigation/NavGraph.kt
@@ -16,6 +16,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.frontend.ui.screen.home.HomeScreen
 import com.frontend.ui.screen.login.LoginScreen
+import com.frontend.ui.screen.walk.WalkScreen
 
 @Composable
 fun NavGraph() {
@@ -36,7 +37,7 @@ fun NavGraph() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = Routes.HOME,
+            startDestination = Routes.WALK,
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(Routes.LOGIN) {
@@ -46,7 +47,7 @@ fun NavGraph() {
                 HomeScreen()
             }
             composable(Routes.WALK) {
-                PlaceholderScreen("산책")
+                WalkScreen()
             }
             composable(Routes.RECORD) {
                 PlaceholderScreen("기록")
