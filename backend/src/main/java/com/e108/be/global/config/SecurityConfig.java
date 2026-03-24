7c9615c5bab92eef07fba566225c2d034b2f8722
김세현 <sshyeoniz@naver.com>
2026-03-24 22:36:45 +0900

    [S14P21E108-259] remove: 채팅 테스í지 허용 설정 제거

--- backend/src/main/java/com/e108/be/global/config/SecurityConfig.java
+++ backend/src/main/java/com/e108/be/global/config/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig {
                         .requestMatchers("/diaries/**").permitAll() // 테스트용 임시 허용
                         .requestMatchers("/error").permitAll()   // Spring 오류 포워딩 허용
                         .requestMatchers("/ws/**").permitAll()   // WebSocket 엔드포인트 허용
-                        .requestMatchers("/chat-test.html").permitAll()  // 채팅 테스트 페이지
 
                         // TODO: 개발 테스트용 임시 허용 - 로그인 구현 완료 후 아래 줄 제거
                         // context-path(/api/v1) 이후 경로만 매칭됨
